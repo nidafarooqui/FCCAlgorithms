888e440dae1fa53652384ada98d4bc6fe78fa92f
Nida <nidafarooqui327@gmail.com>
2016-07-30 21:05:54 +0300

    added a better solution to seek and destroy

--- basic-algorithms/seekanddestroy.js
+++ basic-algorithms/seekanddestroy.js
@@ -28,5 +28,15 @@ function destroyer(arr) {
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
 
+//Much simpler version than mine//
 
+/**
+ function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0, 1);
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
+}
+ */
  
